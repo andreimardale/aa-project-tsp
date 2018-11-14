ec58f73260e309d67805b214172fcc2621f6971e
Andrei MARDALE (66861) <andrei.mardale@stud.acs.upb.ro>
2018-11-14 22:14:43 +0100

    wip

--- src/main/Main.java
+++ src/main/Main.java
@@ -40,28 +40,28 @@ public class Main {
 	public static void main(String[] args) {
 
 		/* Examples of how to use the reading from file infrastructure */
-//		TSPReader tspReader = new TSPReader("wi29.tsp");
-//		TSPInput tspInput = tspReader.read();
-//
-//		AlgorithmDriver driver = new AlgorithmDriver(new DynamicProgrammingTSP());
-//		driver.executeStrategy(tspInput);
+		TSPReader tspReader = new TSPReader("wi29.tsp");
+		TSPInput tspInput = tspReader.read();
+
+		AlgorithmDriver driver = new AlgorithmDriver(new BranchAndBoundTSP());
+		driver.executeStrategy(tspInput);
 //		
 //		driver = new AlgorithmDriver(new BruteForceTSP());
 //		driver.executeStrategy(tspInput);
 //		
-//		driver = new AlgorithmDriver(new BranchAndBoundTSP());
-//		driver.executeStrategy(tspInput);
+		driver = new AlgorithmDriver(new DynamicProgrammingTSP());
+		driver.executeStrategy(tspInput);
 
 //		/* Examples of how to use a hardcoded a graph */
-		TSPInput tspInput2 = new TSPInput("Test Case 1", "TSP", "This is my example test case.", dist3.length, "EUC_2D", dist3);
-		AlgorithmDriver driver2 = new AlgorithmDriver(new BruteForceTSP());
-		driver2.executeStrategy(tspInput2);
-	
-		driver2 = new AlgorithmDriver(new DynamicProgrammingTSP());
-		driver2.executeStrategy(tspInput2);
-
-		driver2 = new AlgorithmDriver(new BranchAndBoundTSP());
-		driver2.executeStrategy(tspInput2);
+//		TSPInput tspInput2 = new TSPInput("Test Case 1", "TSP", "This is my example test case.", dist3.length, "EUC_2D", dist3);
+//		AlgorithmDriver driver2 = new AlgorithmDriver(new BruteForceTSP());
+//		driver2.executeStrategy(tspInput2);
+//	
+//		driver2 = new AlgorithmDriver(new DynamicProgrammingTSP());
+//		driver2.executeStrategy(tspInput2);
+//
+//		driver2 = new AlgorithmDriver(new BranchAndBoundTSP());
+//		driver2.executeStrategy(tspInput2);
 		
 	}
 
