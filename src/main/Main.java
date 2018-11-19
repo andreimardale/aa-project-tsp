637ce1ae5efb6ba062240bc0ff7b3cc0091f1d4e
AlyHdr <43681203+AlyHdr@users.noreply.github.com>
2018-11-19 16:43:01 +0100

    Added GeneticProgrammingTSP

--- src/main/Main.java
+++ src/main/Main.java
@@ -52,6 +52,7 @@ public class Main {
 
 //		/* Examples of how to use a hardcoded a graph */
 		TSPInput tspInput2 = new TSPInput("Test Case 1", "TSP", "This is my example test case.", dist1.length, "EUC_2D", dist1);
+		TSPInput tspInput3 = new TSPReader("eil51.tsp").read();
 		AlgorithmDriver driver2 = new AlgorithmDriver(new BruteForceTSP());
 		driver2.executeStrategy(tspInput2);
 	
@@ -61,11 +62,14 @@ public class Main {
 		driver2 = new AlgorithmDriver(new BranchAndBoundTSP());
 		driver2.executeStrategy(tspInput2);
 		
-		driver2 = new AlgorithmDriver(new MinimumSpanningTreeTSP());
-		driver2.executeStrategy(tspInput2);
+		//driver2 = new AlgorithmDriver(new MinimumSpanningTreeTSP());
+		//driver2.executeStrategy(tspInput3);
 
 		driver2 = new AlgorithmDriver(new GreedyTSP());
-		driver2.executeStrategy(tspInput2);
+		driver2.executeStrategy(tspInput3);
+	
+		driver2 = new AlgorithmDriver(new GeneticProgrammingTSP());
+		driver2.executeStrategy(tspInput3);
 	}
 
 	
